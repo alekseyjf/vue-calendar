128a94a0b7948afc5f16db28dc0d8bbc6c8f58c4
am@artkaidesign.com <developer1>
2018-09-19 18:56:23 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,78 @@
+<template lang="pug">
+  #app
+    .calendar
+      header.calendar__header
+        .active-day
+          .active-day__num 20
+          .active-day__mounth 09
+          .active-day__year 2018
+        form
+          input(type="text")
+
+      main.calendar__main
+        ul.calendar__list
+          day-components
+</template>
+
+<script>
+  import DayComponents from './components/day'
+
+  export default {
+    components: {
+      'day-components': DayComponents
+    },
+    name: 'app',
+    data() {
+      return {
+        msg: 'Welcome to Your Vue App Alex',
+        ms: 'vue-loader',
+
+      }
+    },
+    comenents: {
+      day: {
+        tamplate: '#day'
+      }
+    }
+  }
+</script>
+
+<style>
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+
+  * {
+    box-sizing: border-box;
+  }
+
+  .calendar__header {
+    display: flex;
+  }
+
+  .active-day {
+    display: flex;
+  }
+
+  .active-day__num,
+  .active-day__mounth,
+  .active-day__year {
+    display: flex;
+    font-size: 16px;
+    text-align: left;
+  }
+
+  .active-day__num,
+  .active-day__mounth {
+    width: 25px;
+  }
+
+  .active-day__year {
+    width: 40px;
+  }
+</style>
---
+++ src/components/day.vue
@@ -0,0 +1,94 @@
+<template lang="pug">
+  ul.calendar__list
+    li.calendar__item.prev-month(v-for="itemPrev in +firstDayMonth -1") {{itemPrev}}
+    li.calendar__item( v-for="item, i in +dayInMonth" @click="current(item,i)" :class="{active: flag == i}") {{item}}
+    <!--li.calendar__item( v-for="item in D1Nlast")-->
+
+    li.calendar__item {{ firstDayMonth }}
+    li.calendar__item {{ lastDayPrevMonth }}
+
+    //li.prev-m(v-for="index in D1Nfirst")
+    //li(v-for="day in mounth" v-on:click="current").calendar__item
+      //span.calendar__day {{day}}
+
+  //(v-for="dfirst in D1Nfirst")
+</template>
+<script>
+  let moment = require('moment');
+  export default {
+    data() {
+      return {
+        moment: moment,
+        flag : null,
+        prevMonth: [],
+        prevMonthDay: [],
+        isActive: false,
+
+        firstDayMonth : moment().startOf('month').day(), // первый день месяца
+        lastDayMonth : moment().endOf("month").day(), // последний день месяца
+        dayInMonth : moment().endOf("month").format('DD'),
+        day: moment().endOf("month").format('DD'),
+        dayInPrevMonth : moment("2018/08/07").endOf("month").day(), // последний день месяца
+
+        // D1 : new Date(),
+        // D1lastPrev : new Date(new Date().getFullYear(),new Date().getMonth(),0).getDate(),
+        // D1last : new Date(new Date().getFullYear(),new Date().getMonth()+1,0).getDate(), // последний день месяца
+        // D1Nlast : new Date(new Date().getFullYear(),new Date().getMonth(),new Date(new Date().getFullYear(),new Date().getMonth()+1,0).getDate()).getDay(), // день недели последнего дня месяца
+        // D1Nfirst : new Date(new Date().getFullYear(),new Date().getMonth(),1).getDay() - 1, // день недели первого дня месяца
+      }
+    },
+    methods: {
+      current: function(item,i){
+
+        this.flag = i;
+
+
+        //let date = new Date(year, month)
+            //mounth = date.getMonth();
+        //console.log(el);
+
+        // получить номер дня недели, от 0(пн) до 6(вс)
+        // let totals,
+        //     day = date.getDay();
+        //console.log(day);
+        /*if (day == 0) totals = 7;
+           totals = day + 1;*/
+
+        //console.log(day);
+        //this.isActive = !this.isActive;
+      },
+      currentDay: function(){
+
+      }
+    },
+    created: function(){
+      /*let startDay = this.D1lastPrev - (this.D1Nfirst - 1),
+          lastDay = this.D1lastPrev + 1;
+      for(let i = startDay; i < lastDay; i++){
+        this.prevMonthDay.push(i)
+      }*/
+    }
+  }
+</script>
+<style>
+  .calendar__list {
+    display: flex;
+    flex-wrap: wrap;
+    list-style: none;
+    padding: 0;
+    width: 100%;
+  }
+  .calendar__item {
+    flex: 0 1 calc(100% / 7);
+    width: calc(100% / 7);
+    height: 150px;
+    border: 2px solid #aaa;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-size: 50px;
+  }
+  .prev-month{
+    background-color: #eee;
+  }
+</style>
