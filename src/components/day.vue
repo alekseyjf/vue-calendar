aa02c601cc16109a11a79bc5b33d2fbc3fe7652e
am@artkaidesign.com <developer1>
2018-10-17 10:47:38 +0300

    test configure

--- src/components/day.vue
+++ src/components/day.vue
@@ -1,46 +1,39 @@
 <template lang="pug">
   ul.calendar__list
-    li.calendar__item.prev-month(v-for="itemPrev in +firstDayMonth -1") {{itemPrev}}
-    li.calendar__item( v-for="item, i in +dayInMonth" @click="current(item,i)" :class="{active: flag == i}") {{item}}
-    <!--li.calendar__item( v-for="item in D1Nlast")-->
+    <!--li.calendar__item.prev-month(v-for="itemPrev in +firstDayMonth -1") {{itemPrev}}-->
+    <!--li.calendar__item( v-for="item, i in +dayInMonth" @click="current(item,i)" :class="{active: flag == i}") {{item}}-->
+    li {{ moment().format('LTS') }}
+    <!--li.calendar__item {{ firstDayMonth }}-->
+    <!--li.calendar__item {{ lastDayPrevMonth }}-->
 
-    li.calendar__item {{ firstDayMonth }}
-    li.calendar__item {{ lastDayPrevMonth }}
-
-    //li.prev-m(v-for="index in D1Nfirst")
-    //li(v-for="day in mounth" v-on:click="current").calendar__item
-      //span.calendar__day {{day}}
 
   //(v-for="dfirst in D1Nfirst")
 </template>
 <script>
-  let moment = require('moment');
+
+  //import moment from 'moment'
+
   export default {
     data() {
       return {
-        moment: moment,
+        // moment: moment,
         flag : null,
         prevMonth: [],
         prevMonthDay: [],
         isActive: false,
 
-        firstDayMonth : moment().startOf('month').day(), // первый день месяца
-        lastDayMonth : moment().endOf("month").day(), // последний день месяца
-        dayInMonth : moment().endOf("month").format('DD'),
-        day: moment().endOf("month").format('DD'),
-        dayInPrevMonth : moment("2018/08/07").endOf("month").day(), // последний день месяца
+        // firstDayMonth : moment().startOf('month').day(), // первый день месяца
+        // lastDayMonth : moment().endOf("month").day(), // последний день месяца
+        // dayInMonth : moment().endOf("month").format('DD'),
+        // day: moment().endOf("month").format('DD'),
+        // dayInPrevMonth : moment("2018/08/07").endOf("month").day(), // последний день месяца
 
-        // D1 : new Date(),
-        // D1lastPrev : new Date(new Date().getFullYear(),new Date().getMonth(),0).getDate(),
-        // D1last : new Date(new Date().getFullYear(),new Date().getMonth()+1,0).getDate(), // последний день месяца
-        // D1Nlast : new Date(new Date().getFullYear(),new Date().getMonth(),new Date(new Date().getFullYear(),new Date().getMonth()+1,0).getDate()).getDay(), // день недели последнего дня месяца
-        // D1Nfirst : new Date(new Date().getFullYear(),new Date().getMonth(),1).getDay() - 1, // день недели первого дня месяца
       }
     },
     methods: {
       current: function(item,i){
 
-        this.flag = i;
+        //this.flag = i;
 
 
         //let date = new Date(year, month)
--- src/main.js
+++ src/main.js
@@ -1,7 +1,18 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
+
+// import VueMoment from 'vue-moment'
+import moment from 'moment'
+
+Vue.prototype.moment = moment
 
 new Vue({
   el: '#app',
   render: h => h(App)
-})
+
+});
+
+
+
+//Vue.use( moment )
+
